9e0147f124800152a264a3d04ad9974aae6dd22e
Taai2021 in cyut <taai2021.in.cyut@gmail.com>
2021-02-12 17:13:02 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+
+    window.onscroll = () => {
+      if (window.scrollY >10 || document.body.clientWidth <990) {
+        document.getElementById('van').classList.remove('nav-nobg');
+        document.getElementById('van').classList.add('nav-van');   
+        if  (document.body.clientWidth <990){           
+            document.getElementById('impadw').classList.add('padw');
+        }
+        if  (document.body.clientWidth <750){
+            document.getElementById('banim').src="images/banner02.png";         
+       }
+      }else {
+        document.getElementById('van').classList.remove('nav-van');
+        document.getElementById('van').classList.add('nav-nobg');
+        if  (document.body.clientWidth >=990){           
+          document.getElementById('impadw').classList.remove('padw');
+        }
+        if  (document.body.clientWidth >=750){
+            document.getElementById('banim').src="images/banner03.png";         
+        }
+      }
+    }
+
+    window.onresize = () => {
+      if  (document.body.clientWidth <750){
+        document.getElementById('banim').src="images/banner02.png";         
+      }else{
+        document.getElementById('banim').src="images/banner03.png";
+      }
+      if (window.scrollY >10 || document.body.clientWidth <990) {
+        document.getElementById('van').classList.remove('nav-nobg');
+        document.getElementById('van').classList.add('nav-van');   
+        if  (document.body.clientWidth <990){           
+            document.getElementById('impadw').classList.add('padw');
+        }
+      }else {
+        document.getElementById('van').classList.remove('nav-van');
+        document.getElementById('van').classList.add('nav-nobg');
+        if  (document.body.clientWidth >=990){           
+          document.getElementById('impadw').classList.remove('padw');
+        }        
+      }
+    }
+
+    window.onload = () => {
+      if (window.scrollY >10 || document.body.clientWidth <990) {
+        document.getElementById('van').classList.remove('nav-nobg');
+        document.getElementById('van').classList.add('nav-van');   
+        if  (document.body.clientWidth <990){           
+            document.getElementById('impadw').classList.add('padw');
+        }
+        if  (document.body.clientWidth <750){
+            document.getElementById('banim').src="images/banner02.png";         
+       }
+      }else {
+        document.getElementById('van').classList.remove('nav-van');
+        document.getElementById('van').classList.add('nav-nobg');
+        if  (document.body.clientWidth >=990){           
+          document.getElementById('impadw').classList.remove('padw');
+        }
+        if  (document.body.clientWidth >=750){
+            document.getElementById('banim').src="images/banner03.png";         
+        }
+      }
+    }
